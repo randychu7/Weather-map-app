c2dede9c0f7bdf23a429d170099d9acd9643a854
Randy Chu <randychu7@gmail.com>
2023-01-21 22:43:46 -0600

    lol

---
+++ index.js
@@ -0,0 +1,36 @@
+// $(document).ready(function(){
+
+
+// mapboxgl.accessToken = keys.mapbox;
+// let map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/randychu7/cld4u7lhq000d01o2otr7r35o',
+//     zoom: 10,
+//     center: [-98.4916, 29.4252]
+    
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// });
